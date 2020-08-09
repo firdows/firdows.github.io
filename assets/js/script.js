16fd0cda7d7f1f712e1422ccf03272de4996d157
Ahamad-PC <ahamad.jedu@gmail.com>
2020-08-09 14:48:34 +0700

    Preview

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    $("header.resume-header").load('/includes/header.html');
+$(document).ready(function () {
+    var $loading = $('#loadingDiv').hide();
+    $(document).ajaxStart(function () {
+          $loading.show();
+        //console.log("Loading...");
+    }).ajaxStop(function () {
+        $loading.hide();
+        //console.log("Load Success");
+    });
+
+
+    var pathname = window.location.pathname;
+    var res = pathname.split("/");
+    var lang = res[1] != '' ? "-" + res[1] : '';
+
+    /* Data */
+    $("header.resume-header").load('/includes/header' + lang + '.html');
+    $("section.resume-section").load('/includes/resume-section' + lang + '.html');
+    
+    $("section.experience-section").load('/includes/experience-section' + lang + '.html');
+    $("section.skills-section").load('/includes/skills-section' + lang + '.html');
+
+
 });
